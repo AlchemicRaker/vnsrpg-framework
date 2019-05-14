ebf2cd76b5ae64b9172140bab46660d0d7982453
Zachery Delafosse <natsogames@yahoo.com>
2019-05-14 15:41:14 -0500

    Added a debug-run script, setting background palette, and enabling NMI

---
+++ debugwatch.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+inotifywait -q -m -e close_write *.cfg src/*.s src/*.inc |
+while read -r filename event; do
+	make run
+done
--- src/hello_world.s
+++ src/hello_world.s
@@ -1,17 +1,27 @@
 .include "nes.inc"
 
+.segment "ZEROPAGE"
+
+frame_counter: .res 1
+
 .segment "STATICCODE"
 .proc reset_handler
-	
-	sei				; clear all interrupts
+
+init_interrupts:
+	sei
+init_clear_ppu_state:
 	ldx #$00
 	stx PPUCTRL		; Disable NMI and VRAM increment to 32??
 	stx PPUMASK		; Disable rendering
-	stx $4010		; Disable DMC IRQ??
+init_dmc_state:
+	stx $4010		; Disable DMC IRQ
+init_stack:
 	dex				; x--, = $FF
 	txs				; stack pointer = $01FF
+
 	bit PPUSTATUS	; Acknowledge stray vblank NMI across reset_handler
 	bit SNDCHN		; Acknowledge DMC IRQ
+init_apu:
 	lda #$40
 	sta P2			; Disable APU Frame IRQ
 	lda #$0F
@@ -21,30 +31,47 @@ vwait1:
 	bit PPUSTATUS
 	bpl vwait1
 
+init_clear_ram:
 	cld
-
-	ldx #0
+	ldx #$00
+	; TODO: clear OAM
 	txa
 
 vwait2:
 	bit PPUSTATUS
 	bpl vwait2
-	;jmp main
+	; jmp main_loop
+	
+	lda #VBLANK_NMI
+	sta PPUCTRL
 
-init_color:
-	; set background palette during vblank
+	lda #BG_ON
+	sta PPUMASK
 
-mainloop:
-	jmp mainloop
+init_start_ppu:
+	cli				; Enable interrupts
+
+	lda #$25		
+	sta frame_counter
+
+main_loop:
+	jmp main_loop
 
 .endproc
 
 .proc nmi_handler
-        rti
+	lda #$3F
+	sta PPUADDR
+	lda #$00
+	sta PPUADDR
+	lda frame_counter
+	sta PPUDATA
+	; inc frame_counter ; party mode
+	rti
 .endproc
 
 .proc irq_handler
-        rti
+	rti
 .endproc
 
 .segment "NMIVECTOR"
